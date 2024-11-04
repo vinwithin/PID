e2d3b99385d5ac3ab3b100fbca45334020fc5d93
dimaswisnuprabowo <kelvinadinata82@gmail.com>
2024-11-04 18:24:21 +0700

    feat: beranda

--- resources/views/guest/beranda.blade.php
+++ resources/views/guest/beranda.blade.php
@@ -11,13 +11,38 @@
 </head>
 
 <body>
-    <nav class="navbar">
-        <a href="#">Logo</a>
-        <div class="navbar-extra">
-            <a href="#" id="search"><i data-feather="search"></i></a>
-            <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
-        </div>
-    </nav>
+    <div class="wrapper">
+        <nav class="navbar">
+            <a href="#home">Logo</a>
+            <div class="navbar-extra">
+                <a href="#" id="search"><i data-feather="search"></i></a>
+                {{-- <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a> --}}
+            </div>
+        </nav>
+
+        <section class="hero" id="home">
+            <main class="content">
+                <h1>Pro-IDe Universitas Jambi</h1>
+                <h3>Program Inovasi Berbasis</h3>
+                <h3>Kearifan Lokal Desa</h3>
+                <button>Login</button>
+            </main>
+        </section>
+    </div>
+
+    <section class="publikasi" id="publikasi">
+        <main class="publikasi-content">
+            <h1>Pro-IDe</h1>
+            <p>Pro IDE adalah kegiatan pembinaan dan pemberdayaan masyarakat yang dilakukan oleh
+                mahasiswa melalui Badan Eksekutif Mahasiswa (BEM), Unit Kegiatan Mahasiswa (UKM),
+                HImpunan Mahasiswa (HIMA), dan Organisasi Kemahasiswaan (OK). Program yang sangat bermanfaat baik untuk
+                memajukan desa dan masyarakatnya, juga di
+                desain sebagai bagian dari implementasi Kebijakan Merdeka Belajar Kampus Merdeka (MBKM).
+                Mahasiswa yang tergabung dalam ORMAWA memiliki hak mengikuti pembelajaran di luar
+                program studinya dapat memanfaatkan program ini, melalui berbagai aktivitas membangun desa
+                yang dapat di rekognisi menjadi kegiatan akademik. </p>
+        </main>
+    </section>
 
     <script>
         feather.replace();
